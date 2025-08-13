1c409bfab8134d958640770aa7b05747b00e3a1d
SerGM <sergm080@gmail.com>
2025-08-13 14:25:52 +0300

    fix the background path

---
+++ swiper.js
@@ -0,0 +1,20 @@
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  slidesPerView: 3,
+  spaceBetween: 16,
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+
+});
